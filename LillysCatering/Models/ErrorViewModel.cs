540ab442b0780e6218543f28e016576d8158a5c9
Jenzi <jennydinh94@gmail.com>
2023-03-01 09:21:49 +0100

    First commit

---
+++ LillysCatering/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LillysCatering.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
