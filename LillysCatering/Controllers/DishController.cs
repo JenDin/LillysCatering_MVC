1d244a467415f5a5b54cc2468507cc4508b95596
Jenzi <jennydinh94@gmail.com>
2023-03-01 09:33:51 +0100

    Scaffold dish controller and views

---
+++ LillysCatering/Controllers/DishController.cs
@@ -0,0 +1,168 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LillysCatering.Data;
+using LillysCatering.Models;
+
+namespace LillysCatering.Controllers
+{
+    public class DishController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DishController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Dish
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Dishes.Include(d => d.Menu);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Dish/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Dishes == null)
+            {
+                return NotFound();
+            }
+
+            var dish = await _context.Dishes
+                .Include(d => d.Menu)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (dish == null)
+            {
+                return NotFound();
+            }
+
+            return View(dish);
+        }
+
+        // GET: Dish/Create
+        public IActionResult Create()
+        {
+            ViewData["MenuId"] = new SelectList(_context.Menus, "Id", "Title");
+            return View();
+        }
+
+        // POST: Dish/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Ingredients,MenuId")] Dish dish)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(dish);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MenuId"] = new SelectList(_context.Menus, "Id", "Title", dish.MenuId);
+            return View(dish);
+        }
+
+        // GET: Dish/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Dishes == null)
+            {
+                return NotFound();
+            }
+
+            var dish = await _context.Dishes.FindAsync(id);
+            if (dish == null)
+            {
+                return NotFound();
+            }
+            ViewData["MenuId"] = new SelectList(_context.Menus, "Id", "Title", dish.MenuId);
+            return View(dish);
+        }
+
+        // POST: Dish/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Ingredients,MenuId")] Dish dish)
+        {
+            if (id != dish.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(dish);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DishExists(dish.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MenuId"] = new SelectList(_context.Menus, "Id", "Title", dish.MenuId);
+            return View(dish);
+        }
+
+        // GET: Dish/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Dishes == null)
+            {
+                return NotFound();
+            }
+
+            var dish = await _context.Dishes
+                .Include(d => d.Menu)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (dish == null)
+            {
+                return NotFound();
+            }
+
+            return View(dish);
+        }
+
+        // POST: Dish/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Dishes == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Dishes'  is null.");
+            }
+            var dish = await _context.Dishes.FindAsync(id);
+            if (dish != null)
+            {
+                _context.Dishes.Remove(dish);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DishExists(int id)
+        {
+          return (_context.Dishes?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
